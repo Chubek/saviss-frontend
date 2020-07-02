01b88db8c51144d7ea97a2e04c06f4eba817bca1
Sethu <Sethu>
2020-07-03 03:24:55 +0530

    new toast

--- api/pool/_getPool.js
+++ api/pool/_getPool.js
@@ -1,21 +1,23 @@
-import axios from "axios"
-import {toast} from "@wrappers/toast";
+import axios from "axios";
+import { toast } from "@wrappers/toast";
 import Constants from "expo-constants";
 
 export const _getPool = async (listenerToken) => {
-    try {
-        const poolRes = await axios.get(`${Constants.manifest.extra.serverUrl}/pool/get/`, {headers: {"x-auth-token-listener": listenerToken}});
+  try {
+    const poolRes = await axios.get(
+      `${Constants.manifest.extra.serverUrl}/pool/get/`,
+      { headers: { "x-auth-token-listener": listenerToken } }
+    );
 
-
-        if (poolRes.status === 200 || poolRes.status === 303) {
-            return poolRes.data.pool;
-        }
-    } catch (e) {
-        if (e.response.status === 404) {
-            toast("Nothing to show");
-        }
-
-        throw e;
+    if (poolRes.status === 200 || poolRes.status === 303) {
+      return poolRes.data.pool;
     }
-}
+  } catch (e) {
+    if (e.response.status === 404) {
+      toast("Nothing to show");
+    }
+    toast("Sorry something went wrong");
 
+    // throw e;
+  }
+};
