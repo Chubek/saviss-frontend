be2b22065713310a39968101269a092db9ddd0a6
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-07-03 10:27:06 +0430

    fixed the screw up

--- components/globalStyles.js
+++ components/globalStyles.js
@@ -1,43 +1,6 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from "react-native";
 
 export default styles = StyleSheet.create({
-<<<<<<< HEAD
-    bg: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "space-around",
-        width: "100%",
-        height: "100%"
-    },
-    input: {
-        color: "#fff",
-        borderBottomWidth: 20,
-    },
-    label: {
-        color: "#582630",
-        textShadowColor: "#F1A66A",
-        textShadowOffset: {width: -1, height: 1},
-        textShadowRadius: 13,
-    },
-    buttonContainerStyle: {
-        backgroundColor: "#124",
-        borderRadius: 20,
-        width: "80%",
-        alignSelf: "center"
-    },
-    buttonStyle: {
-        backgroundColor: "#124",
-        borderRadius: 20,
-    },
-    chatHeader: {
-        backgroundColor: "#f01"
-    },
-    adSpace: {
-        backgroundColor: "#f12"
-    },
-    waitingTextStyle: {
-        color: "#fff"
-    },
     container: {
         marginTop: 20,
         marginLeft: 10,
@@ -83,40 +46,38 @@ export default styles = StyleSheet.create({
     },
     link: {
         color: "blue"
-    }
+    },
 
-=======
-  bg: {
-    flex: 1,
-    alignItems: "center",
-  },
-  input: {
-    color: "#fff",
-    borderBottomWidth: 20,
-  },
-  label: {
-    color: "#582630",
-    textShadowColor: "#F1A66A",
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 13,
-  },
-  buttonContainerStyle: {
-    backgroundColor: "#124",
-    width: "80%",
-    alignSelf: "center",
-  },
-  buttonStyle: {
-    backgroundColor: "#124",
-    borderRadius: 20,
-  },
-  chatHeader: {
-    backgroundColor: "#f01",
-  },
-  adSpace: {
-    backgroundColor: "#f12",
-  },
-  waitingTextStyle: {
-    color: "#fff",
-  },
->>>>>>> 01b88db8c51144d7ea97a2e04c06f4eba817bca1
+    bg: {
+        flex: 1,
+        alignItems: "center",
+    },
+    input: {
+        color: "#fff",
+        borderBottomWidth: 20,
+    },
+    label: {
+        color: "#582630",
+        textShadowColor: "#F1A66A",
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 13,
+    },
+    buttonContainerStyle: {
+        backgroundColor: "#124",
+        width: "80%",
+        alignSelf: "center",
+    },
+    buttonStyle: {
+        backgroundColor: "#124",
+        borderRadius: 20,
+    },
+    chatHeader: {
+        backgroundColor: "#f01",
+    },
+    adSpace: {
+        backgroundColor: "#f12",
+    },
+    waitingTextStyle: {
+        color: "#fff",
+    },
 });
